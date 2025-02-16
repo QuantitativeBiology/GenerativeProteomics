fix hint matrix computation for default run, fix imputation evaluation output for default matrix, added default seeds for result replication

ProtoGain/dataset.py
@@ -34,10 +34,8 @@ class Data:
             dataset = dataset.T
             dataset = dataset.values
             missingness = 1 - mask.mean(axis=1)
-        #########################################
 
         # MinMaxScaler
-
         self.scaler = MinMaxScaler()
         # self.scaler_T = MinMaxScaler()
 
@@ -95,13 +93,17 @@ class Data:
 
         if axis == "columns":
             df_dummy = df_dummy.fillna(df_dummy.mean(axis=0))
+            df_dummy = df_dummy.fillna(0)
             cls.ref_missingness = 1 - cls.ref_mask.mean(axis=0)
+
         elif axis == "rows":
             df_dummy = df_dummy.T.fillna(df_dummy.mean(axis=1))
+            df_dummy = df_dummy.T.fillna(0)
             df_dummy = df_dummy.T
             cls.ref_missingness = 1 - cls.ref_mask.mean(axis=1)
 
         cls.ref_mean_imputed = torch.tensor(df_dummy.values)
+        cls.ref_dataset = torch.tensor(df_dummy.values)
 
         cls.ref_hint = generate_hint(cls.ref_mask, params.hint_rate)
         cls.ref_dataset_scaled = torch.from_numpy(cls.scaler.transform(cls.ref_dataset))
@@ -176,8 +178,15 @@ def generate_hint_paper(mask, hint_rate, missingness):
     return hint
 
 
-def generate_hint_paper_missingness(mask, hint_rate, missingness):
+def generate_hint_paper_missingness(mask, hint_rate, missingness, transpose, mb_idx):
     hint_mask = generate_mask(mask, 1 - hint_rate)
-    hint = mask * hint_mask + np.multiply((1 - missingness), (1 - hint_mask).T).T
+
+    if transpose == 1:
+        hint = (
+            mask * hint_mask
+            + np.multiply((1 - missingness[mb_idx]), (1 - hint_mask).T).T
+        )
+    elif transpose == 0:
+        hint = mask * hint_mask + np.multiply((1 - missingness), (1 - hint_mask))
 
     return hint

ProtoGain/model.py
@@ -110,7 +110,7 @@ class Network:
                 if cls.hypers.miss_forest == 1:
                     ref_imputed[i] = np.array(
                         [
-                            data.dataset_T[tuple(id)],
+                            data.dataset[tuple(id)],
                             cls.metrics.ref_data_imputed[tuple(id)],
                             id[0],
                             id[1],
@@ -121,7 +121,7 @@ class Network:
                 elif cls.hypers.miss_forest == 0:
                     ref_imputed[i] = np.array(
                         [
-                            data.dataset_T[tuple(id)],
+                            data.dataset[tuple(id)],
                             cls.metrics.ref_data_imputed[tuple(id)],
                             id[0],
                             id[1],
@@ -377,10 +377,11 @@ class Network:
         for it in pbar:
 
             train_batch, train_mask_batch, train_hint_batch, mean_batch = (
-                cls._create_batch(data_train, mask_train, missingness, mean)
+                cls._create_batch(data_train, mask_train, missingness, mean, transpose)
             )
+
             test_batch, test_mask_batch, _, _ = cls._create_batch(
-                data_test, mask_test, missingness, mean
+                data_test, mask_test, missingness, mean, transpose
             )
 
             sample_G = cls.generate_sample(train_batch, train_mask_batch)
@@ -458,7 +459,7 @@ class Network:
         for it in pbar:
 
             batch, mask_batch, hint_batch = cls._create_batch(
-                data_train, mask_train, missingness
+                data_train, mask_train, missingness, transpose
             )
 
             cls._calculate_losses_train(it, batch, mask_batch, hint_batch)
@@ -490,7 +491,7 @@ class Network:
                 cls.hypers.override,
             )
 
-    def _create_batch(cls, dataset, mask, missingness, mean=None):
+    def _create_batch(cls, dataset, mask, missingness, mean=None, transpose=None):
 
         mb_idx = utils.sample_idx(dataset.shape[0], cls.hypers.batch_size)
 
@@ -498,7 +499,7 @@ class Network:
         mask_batch = mask[mb_idx].detach().clone()
         # train_hint_batch = hint[mb_idx].detach().clone()
         hint_batch = generate_hint_paper_missingness(
-            mask_batch, cls.hypers.hint_rate, missingness[mb_idx]
+            mask_batch, cls.hypers.hint_rate, missingness, transpose, mb_idx
         )
 
         if mean is not None:

ProtoGain/protogain.py
@@ -22,6 +22,9 @@ import argparse
 import os
 import psutil
 
+torch.manual_seed(42)
+np.random.seed(42)
+
 
 def init_arg():
     parser = argparse.ArgumentParser()
@@ -129,6 +132,9 @@ if __name__ == "__main__":
         output_all = params.output_all
         miss_forest = params.miss_forest
 
+        project = args.project
+        cores = args.cores
+
     else:
         params = Params(
             missing_file,
@@ -224,7 +230,7 @@ if __name__ == "__main__":
 
         data = Data(missing, params, axis="columns")
 
-        # model.evaluation_run(data, missing_header, transpose=0)
+        model.evaluation_run(data, missing_header, transpose=0)
         # model.train(data, missing_header, transpose=0)
 
         ################ Starting Transpose run ################
@@ -261,7 +267,7 @@ if __name__ == "__main__":
         model = Network(hypers=params, net_G=net_G, net_D=net_D, metrics=metrics)
 
         model.evaluation_run(data, missing_header, transpose=1)
-        model.train(data, missing_header, transpose=1)
+        # model.train(data, missing_header, transpose=1)
 
         #########################################################################
 