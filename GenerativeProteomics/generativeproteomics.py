9af7c748e98861f93344cd679811e0806036b3ca
DiogoCSTF <diogotavfer2002@gmail.com>
2025-08-23 16:42:28 +0100

    feat: add hugging function

--- GenerativeProteomics/generativeproteomics.py
+++ GenerativeProteomics/generativeproteomics.py
@@ -45,6 +45,7 @@ def init_arg():
         "--override", type=int, default=0, help="override previous files"
     )
     parser.add_argument("--outall", type=int, default=0, help="output all files")
+    parser.add_argument("--model", type = str, help = "indicates which model from HuggingFace to use")
     return parser.parse_args()
 
 
@@ -70,130 +71,147 @@ if __name__ == "__main__":
         parameters_file = args.parameters
         override = args.override
         output_all = args.outall
+        model = args.model
 
-        if parameters_file is not None:
-            params = Params.read_hyperparameters(parameters_file)
-            missing_file = params.input
-            output_file = params.output
-            ref_file = params.ref
-            output_folder = params.output_folder
-            num_iterations = params.num_iterations
-            batch_size = params.batch_size
-            alpha = params.alpha
-            miss_rate = params.miss_rate
-            hint_rate = params.hint_rate
-            lr_D = params.lr_D
-            lr_G = params.lr_G
-            override = params.override
-            output_all = params.output_all
+        if model is not None:
+            utils.hugging_face(model, missing_file)
 
         else:
-            params = Params(
-                missing_file,
-                output_file,
-                ref_file,
-                output_folder,
-                None,
-                num_iterations,
-                batch_size,
-                alpha,
-                miss_rate,
-                hint_rate,
-                lr_D,
-                lr_G,
-                override,
-                output_all,
-            )
 
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        if missing_file is None:
-            print("Input file not provided")
-            exit(1)
-        if missing_file.endswith(".csv"):
-            df_missing = pd.read_csv(missing_file)
-            missing = df_missing.values
-            missing_header = df_missing.columns.tolist()
-            params.update_hypers(header=missing_header)
-        elif missing_file.endswith(".tsv"):
-            df_missing = utils.build_protein_matrix(missing_file)
-            missing = df_missing.values
-            missing_header = df_missing.columns.tolist()
-            params.update_hypers(header=missing_header)
-        else:
-            print("Invalid file format")
-            exit(2)
-        dim = missing.shape[1]
-        train_size = missing.shape[0]
-
-        h_dim1 = dim
-        h_dim2 = dim
-
-        net_G = nn.Sequential(
-            nn.Linear(dim * 2, h_dim1),
-            nn.ReLU(),
-            nn.Linear(h_dim1, h_dim2),
-            nn.ReLU(),
-            nn.Linear(h_dim2, dim),
-            nn.Sigmoid(),
-        )
-
-        net_D = nn.Sequential(
-            nn.Linear(dim * 2, h_dim1),
-            nn.ReLU(),
-            nn.Linear(h_dim1, h_dim2),
-            nn.ReLU(),
-            nn.Linear(h_dim2, dim),
-            nn.Sigmoid(),
-        )
-
-        metrics = Metrics(params)
-        model = Network(hypers=params, net_G=net_G, net_D=net_D, metrics=metrics)
-
-        if ref_file is not None:
-            df_ref = pd.read_csv(ref_file)
-            ref = df_ref.values
-            ref_header = df_ref.columns.tolist()
-
-            if dim != ref.shape[1]:
-                print(
-                    "\n\nThe reference and data files provided don't have the same number of features\n"
-                )
-                exit(3.1)
-            elif train_size != ref.shape[0]:
-                print(
-                    "\n\nThe reference and data files provided don't have the same number of samples\n"
-                )
-                exit(3.2)
-
-            data = Data(missing, miss_rate, hint_rate, ref)
-            model.train_ref(data, missing_header)
-
-        else:
-            data = Data(missing, miss_rate, hint_rate)
-            model.evaluate(data, missing_header)
-            model.train(data, missing_header)
-
-        run_time = []
-        run_time.append(time.time() - start_time)
-        file_path = output_folder + "run_time.csv"
-
-        if override == 1:
-            df_run_time = pd.DataFrame(run_time)
-            df_run_time.to_csv(file_path, index=False)
-
-        else:
-            if os.path.exists(file_path):
-                with open(file_path, "a") as myfile:
-                    myfile.write(str(run_time[0]) + "\n")
+            if parameters_file is not None:
+                params = Params.read_hyperparameters(parameters_file)
+                missing_file = params.input
+                output_file = params.output
+                ref_file = params.ref
+                output_folder = params.output_folder
+                num_iterations = params.num_iterations
+                batch_size = params.batch_size
+                alpha = params.alpha
+                miss_rate = params.miss_rate
+                hint_rate = params.hint_rate
+                lr_D = params.lr_D
+                lr_G = params.lr_G
+                override = params.override
+                output_all = params.output_all
 
             else:
+                params = Params(
+                    missing_file,
+                    output_file,
+                    ref_file,
+                    output_folder,
+                    None,
+                    num_iterations,
+                    batch_size,
+                    alpha,
+                    miss_rate,
+                    hint_rate,
+                    lr_D,
+                    lr_G,
+                    override,
+                    output_all,
+                )
+
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+
+            if missing_file is None:
+                print("Input file not provided")
+                exit(1)
+            if missing_file.endswith(".csv"):
+                df_missing = pd.read_csv(missing_file)
+                missing = df_missing.values
+                missing_header = df_missing.columns.tolist()
+                params.update_hypers(header=missing_header)
+            elif missing_file.endswith(".tsv"):
+                df_missing = utils.build_protein_matrix(missing_file)
+                missing = df_missing.values
+                missing_header = df_missing.columns.tolist()
+                params.update_hypers(header=missing_header)
+            elif missing_file.endswith(".parquet"):
+                print("Reading parquet file")
+                df_missing = utils.handle_parquet(missing_file)
+                print("Parquet file read - handle_parquet ")
+                print(df_missing)
+                missing = df_missing.to_numpy()
+                print("Parquet file read - to_numpy  done")
+                missing_header = df_missing.columns
+                print("Parquet file read - columns done")
+                params.update_hypers(header=missing_header)
+                print("Parquet file read - update_hypers done")
+            else:
+                print("Invalid file format")
+                exit(2)
+            dim = missing.shape[1]
+            train_size = missing.shape[0]
+
+            h_dim1 = dim
+            h_dim2 = dim
+
+            net_G = nn.Sequential(
+                nn.Linear(dim * 2, h_dim1),
+                nn.ReLU(),
+                nn.Linear(h_dim1, h_dim2),
+                nn.ReLU(),
+                nn.Linear(h_dim2, dim),
+                nn.Sigmoid(),
+            )
+
+            net_D = nn.Sequential(
+                nn.Linear(dim * 2, h_dim1),
+                nn.ReLU(),
+                nn.Linear(h_dim1, h_dim2),
+                nn.ReLU(),
+                nn.Linear(h_dim2, dim),
+                nn.Sigmoid(),
+            )
+
+            metrics = Metrics(params)
+            model = Network(hypers=params, net_G=net_G, net_D=net_D, metrics=metrics)
+
+            if ref_file is not None:
+                df_ref = pd.read_csv(ref_file)
+                ref = df_ref.values
+                ref_header = df_ref.columns.tolist()
+
+                if dim != ref.shape[1]:
+                    print(
+                        "\n\nThe reference and data files provided don't have the same number of features\n"
+                    )
+                    exit(3.1)
+                elif train_size != ref.shape[0]:
+                    print(
+                        "\n\nThe reference and data files provided don't have the same number of samples\n"
+                    )
+                    exit(3.2)
+
+                data = Data(missing, miss_rate, hint_rate, ref)
+                model.train_ref(data, missing_header)
+
+            else:
+                data = Data(missing, miss_rate, hint_rate)
+                model.evaluate(data, missing_header)
+                model.train(data, missing_header)
+
+            run_time = []
+            run_time.append(time.time() - start_time)
+            file_path = output_folder + "run_time.csv"
+
+            if override == 1:
                 df_run_time = pd.DataFrame(run_time)
                 df_run_time.to_csv(file_path, index=False)
 
-    print("\n--- %s seconds ---\n\n" % (run_time[0]))
-    results = pstats.Stats(profile)
-    results.sort_stats(pstats.SortKey.TIME)
-    # results.print_stats()
-    results.dump_stats("results.prof")
+            else:
+                if os.path.exists(file_path):
+                    with open(file_path, "a") as myfile:
+                        myfile.write(str(run_time[0]) + "\n")
+
+                else:
+                    df_run_time = pd.DataFrame(run_time)
+                    df_run_time.to_csv(file_path, index=False)
+
+            print("\n--- %s seconds ---\n\n" % (run_time[0]))
+            results = pstats.Stats(profile)
+            results.sort_stats(pstats.SortKey.TIME)
+            # results.print_stats()
+            results.dump_stats("results.prof")
--- GenerativeProteomics/utils.py
+++ GenerativeProteomics/utils.py
@@ -1,10 +1,12 @@
+from sklearn.preprocessing import LabelEncoder
+from huggingface_hub import hf_hub_download
 import torch
 import numpy as np
 import pandas as pd
 import os
 import anndata as ad
 import polars as pl
-
+import json
 
 def create_csv(data, name: str, header):
     df = pd.DataFrame(data)
@@ -170,3 +172,75 @@ def handle_parquet(parquet_file):
     #dataset = numeric_cols.to_numpy(dtype=np.float32)
 
     return df_numeric
+
+def hugging_face(model, dataset_path):
+
+    """ Function to import and use the classes and functions of the GenerativeProteomics package. """
+
+    save_dir = "./GAIN_DANN_model"
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Download files manually
+    config_path = hf_hub_download(repo_id = f"QuantitativeBiology/{model}", filename="config.json", cache_dir = save_dir)
+    weights_path = hf_hub_download(repo_id =f"QuantitativeBiology/{model}", filename="pytorch_model.bin", cache_dir = save_dir)
+    model_path = hf_hub_download(repo_id = f"QuantitativeBiology/{model}", filename="modeling_gain_dann.py", cache_dir = save_dir)
+
+    print("config_path:", config_path)
+    print("weights_path:", weights_path)
+    print("model_path", model_path)
+
+    directory = os.path.dirname(model_path)
+    print("directory:", directory)
+
+    import sys
+    sys.path.append(directory)  # Add the directory containing 'modeling_gain_dann.py' to the Python path
+
+    if model == "GAIN_DANN_model":
+
+    # Now you can import the functions or classes inside the file
+        from modeling_gain_dann import GainDANNConfig, GainDANN
+
+
+        print("import successfully done")
+
+        # Load config
+        with open(config_path) as f:
+            cfg = json.load(f)
+
+        hela = pd.read_csv(dataset_path, index_col=0)
+        #hela_numeric = hela.drop(columns=['Project'])
+
+        input_dim = hela.shape[1]
+        
+        print(input_dim)
+        cfg['input_dim'] = input_dim
+
+        config = GainDANNConfig(**cfg)
+        model = GainDANN(config)
+
+        # Load raw state_dict
+        state_dict = torch.load(weights_path, map_location="cpu")
+
+        # Add "model." prefix to every key(because state_dict has the weights of GAIN_DANN but I added the Gain_DANN for the huggingFace accordance)
+        renamed_state_dict = {f"model.{k}": v for k, v in state_dict.items()}
+
+        # Load with corrected keys
+        model.load_state_dict(renamed_state_dict)
+
+        model.eval()
+
+        print(hela.dtypes)
+
+        label_encoder = LabelEncoder()
+        hela['Project'] = label_encoder.fit_transform(hela['Project'])
+
+        x = torch.tensor(hela.values, dtype=torch.float32)
+
+        with torch.no_grad():
+            x_reconstructed, x_domain = model(x)
+
+        print("x_reconstructed:", x_reconstructed)
+        print("x_domain:", x_domain)
+
+        pd.DataFrame(x_reconstructed.numpy()).to_csv("x_reconstructed.csv", index=False)
+        pd.DataFrame(x_domain.numpy()).to_csv("x_domain.csv", index=False)
