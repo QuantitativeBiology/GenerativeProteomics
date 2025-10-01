3bcc96a1d2313e297ae4eafe70a9b15f50829fde
DiogoCSTF <diogotavfer2002@gmail.com>
2025-10-01 15:30:37 +0100

    fix: new abstract class for constant API of imputation Methods

--- GenerativeProteomics/imputation_management.py
+++ GenerativeProteomics/imputation_management.py
@@ -1,18 +1,14 @@
-from sklearn.preprocessing import LabelEncoder
-from huggingface_hub import hf_hub_download
-import torch
-import os
-import json
-import pandas as pd
-import argparse
-import numpy as np
+from models.base_abstract import ImputationModel
+from models.gain_dann import GainDannImputationModel
+from models.medium import MediumImputationModel
+
 
 class Imputation_Management:
     def __init__ (self, model, df_missing, missing_file_path):
         self.model = model
         self.df = df_missing
         self.missing = missing_file_path
-        self.dict_imputation_methods = {"GAIN_DANN_model" : hugging_face_gain_dann, "medium_imputation" : medium_imputation}
+        self.dict_imputation_methods = {"GAIN_DANN_model" : GainDannImputationModel(), "medium_imputation" : MediumImputationModel()}
 
 
     def add_method(self, model, fn): 
@@ -26,89 +22,14 @@ class Imputation_Management:
         if model not in self.dict_imputation_methods:
             raise SystemExit (f"Unknown model called {model}, Models available are: {','.join(self.dict_imputation_methods)}")
         else:
-            return self.dict_imputation_methods[model](self.df)
+            return self.dict_imputation_methods[model].run(self.df)
 
 
 
 
-def hugging_face_gain_dann(dataset_path):
-
-    """ Function to import and use the GAIN_DANN model from Hugging Face. """
-
-    save_dir = "./GAIN_DANN_model"
-    os.makedirs(save_dir, exist_ok=True)
-
-    # Download files manually
-    config_path = hf_hub_download(repo_id = "QuantitativeBiology/GAIN_DANN_model", filename="config.json", cache_dir = save_dir)
-    weights_path = hf_hub_download(repo_id =f"QuantitativeBiology/GAIN_DANN_model", filename="pytorch_model.bin", cache_dir = save_dir)
-    model_path = hf_hub_download(repo_id = f"QuantitativeBiology/GAIN_DANN_model", filename="modeling_gain_dann.py", cache_dir = save_dir)
-
-    directory = os.path.dirname(model_path)
-
-    # Add the directory containing 'modeling_gain_dann.py' to the Python path
-    import sys
-    sys.path.append(directory)  
-
-    # Import the functions or classes inside the file
-    from modeling_gain_dann import GainDANNConfig, GainDANN
-
-    print("import successfully done")
-
-    # Load config
-    with open(config_path) as f:
-        cfg = json.load(f)
-
-    hela = dataset_path.iloc[:, 1:]
-    input_dim = hela.shape[1]
-    
-    print(input_dim)
-    cfg['input_dim'] = input_dim
-
-    config = GainDANNConfig(**cfg)
-    model = GainDANN(config)
-
-    # Load raw state_dict
-    state_dict = torch.load(weights_path, map_location="cpu")
-
-    # Add "model." prefix to every key(because state_dict has the weights of GAIN_DANN but I added the Gain_DANN for the huggingFace accordance)
-    renamed_state_dict = {f"model.{k}": v for k, v in state_dict.items()}
-
-    # Load with corrected keys
-    model.load_state_dict(renamed_state_dict)
-
-    model.eval()
-
-    label_encoder = LabelEncoder()
-    hela['Project'] = label_encoder.fit_transform(hela['Project'])
-
-    x = torch.tensor(hela.values, dtype=torch.float32)
-
-    with torch.no_grad():
-        x_reconstructed, x_domain = model(x)
-
-    print("x_reconstructed:", x_reconstructed)
-    print("x_domain:", x_domain)
-
-    pd.DataFrame(x_reconstructed.numpy()).to_csv("x_reconstructed.csv", index=False)
-    pd.DataFrame(x_domain.numpy()).to_csv("x_domain.csv", index=False)
 
 
 
 
-def medium_imputation(dataset):
-
-    '''Function that calculates the medium value of each column and imputes missing values with 
-    that value '''
-
-    df = dataset.copy()
-    num_cols = df.select_dtypes(include=[np.number]).columns
-
-    df[num_cols] = df[num_cols].astype(float).replace(0, np.nan)
-
-    col_means = df[num_cols].mean().fillna(0.0)
-
-    df[num_cols] = df[num_cols].fillna(col_means)
-    print(df)
-    return df
 
 
